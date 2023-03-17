bc36e75d3ade1a9e7d54a92da716f042a7bf8e5b
Andrew OBrien <aobrienaf@gmail.com>
2023-03-16 21:04:05 -0500

    Andrews Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name : "Sam",
+    streetAddress : "11 Broadway",        
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployees = {...employee};
+    newEmployees[key] = value;
+   return (employee, newEmployees);
+   }
+   function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployees = employee;
+    newEmployees[key] = value;
+   return (newEmployees);
+   }
+   function deleteFromEmployeeByKey(employee, key, value) {
+    let newEmployees = {...employee};
+    newEmployees[key] = value;
+    delete newEmployees.key;
+   return (employee, newEmployees);
+   }
+   function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return (employee);
+   }
